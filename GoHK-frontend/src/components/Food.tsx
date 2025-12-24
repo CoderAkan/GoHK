9114e5a01e93a79da5701ec3385e7f7b08884321
Imanzhussip <your.email@example.com>
2025-12-24 18:47:07 +0800

    Food h/w fiexd

--- GoHK-frontend/src/components/Food.tsx
+++ GoHK-frontend/src/components/Food.tsx
@@ -2,10 +2,10 @@ import type {FC} from 'react'
 import ReactStars from "react-stars";
 
 import { IoLocationOutline } from "react-icons/io5";
-import { type rest } from '../Types/accomodation_rest_types';
+import { type rest } from '../Types/accomodation_rest_place_types';
 import dicos from '../assets/Food/dicos.jpeg'
 import ICC from "../assets/Food/IslamCentreCanteen.jpeg"
-import Ebik from "../assets/Food/ebeneezers.jpg"
+import Ebik from "../assets/Food/ebeneezers.jpeg"
 import waikee from "../assets/Food/wai-kee-1920x1080-b.jpg"
 import pakeeza from "../assets/Food/pakeeza.jpeg"
 import ifink from "../assets/Food/islam-food-in-kowloon.jpg"
@@ -15,7 +15,7 @@ const Food: FC = () => {
       image: dicos,
       name: "Dicos",
       description: "Founded in 1994, Dicos rivals KFC and McDonald's, serving distinctive fried chicken and burgers, with halal options for travelers worldwide.",
-      address: "Seng Ming Court, 375-377 King's Rd, North Point, Гонконг",
+      address: "Seng Ming Court",
       rating: 3.6,
       price: 50,
       cuisine: "Chinese-style fast food",
@@ -27,7 +27,7 @@ const Food: FC = () => {
       image: ICC,
       name: "Islam Centre Canteen",
       description: "Popular Wan Chai canteen serving fully halal Cantonese food, famous for authentic dim sum, plus rice and noodle dishes locals.",
-      address: "Гонконг, Wan Chai, Oi Kwan Rd, 40號伊斯蘭中心5樓",
+      address: "Wan Chai",
       rating: 4.5,
       price: 75,
       cuisine: "Cantonese food",
@@ -39,7 +39,7 @@ const Food: FC = () => {
       image: Ebik,
       name: "Ebeneezer's Restaurant & Bar (Hollywood Road) ",
       description: "This Middle-east cuisine restaurant opened in 1993, and there are many branches in different districts. It mainly offers kebab and curry with reasonable prices.",
-      address: "G/F, 24 Hollywood Road, Soho, Central",
+      address: "Central",
       rating: 4.9,
       price: 150,
       cuisine: "",
@@ -51,7 +51,7 @@ const Food: FC = () => {
       image: waikee,
       name: "Wai Kee",
       description: "Popular halal Cantonese restaurant in Hong Kong, famous for roast duck, beef brisket noodles, generous portions, and affordable comfort food.",
-      address: "Shop 5, Bowrington Road Cooked Food Centre, 1/F, Bowrington Road Market, 21 Bowrington Road, Wan Chai",
+      address: "Wan Chai",
       rating: 3.7,
       price: 50,
       cuisine: "Halal Cantonese cuisine",
@@ -63,7 +63,7 @@ const Food: FC = () => {
       image: pakeeza,
       name: "Pakeeza",
       description: "Pakeeza is a Hong Kong restaurant serving authentic Pakistani and Indian halal cuisine, known for rich curries, biryanis, and hospitality",
-      address: "Shop 51, 2nd Floor, Mirador Mansion, 58 Nathan Road, Tsim Sha Tsui, Hong Kong China",
+      address: "Tsim Sha Tsui",
       rating: 5,
       price: 150,
       cuisine: "Pakistani and North Indian cuisine",
@@ -75,7 +75,7 @@ const Food: FC = () => {
       image: ifink,
       name: "Islam Food In Kowloon City",
       description: "Islamic restaurants in Kowloon City offer halal beef noodles, lamb curries, naan, friendly service, strong Muslim heritage atmosphere and community.",
-      address: "G/F, 1 Lung Kong Road, Kowloon City",
+      address: "Kowloon City",
       rating: 4.1,
       price: 125,
       cuisine: "Halal Asian / Hong Kong-style Chinese food",
@@ -143,7 +143,7 @@ const Food: FC = () => {
             <div> {/* list of hotels and hostels */}
               {restaurants.map((rest, index) => (
                 <div key={index} className='flex mt-5 first:mt-0 border rounded-lg items-stretch border-black/10 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_4px_6px_-1px_rgba(0,0,0,0.1)]'>
-                  <img className='rounded-l-lg' src={rest.image} alt="" height={150} width={350} />
+                  <img src={rest.image} alt="" className="w-[400px] h-[200px] object-cover rounded-l-lg flex-shrink-0" />
                   <div className='flex text-xs justify-between mb-4 flex-col mt-4 mx-10'>
                     <div className='flex justify-between items-end'>
                       <p className='font-medium text-xl'>{rest.name}</p>
